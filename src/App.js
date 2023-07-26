2b8fbae7f6044862bc40fad0a92e8325d9f457ff
Sergey <sergsmile29@gmail.com>
2023-07-26 22:26:20 +0300

    styled

--- src/App.js
+++ src/App.js
@@ -1,39 +1,5 @@
 import React from 'react'
-import { Header } from './components/Header'
-import { Section } from './components/Section'
-import { Wrapper } from './components/Wrapper'
 
 export const App = () => {
-	const moviesData = [
-		{ id: 1, title: 'Terminator' },
-		{ id: 2, title: 'Taxi' },
-		{ id: 3, title: 'StepUp' },
-		{ id: 4, title: 'TEst123' },
-	]
-	const goodsData = [
-		{ id: 1, title: 'Phone' },
-		{ id: 2, title: 'Cabbage' },
-		{ id: 3, title: 'Milk' },
-	]
-	const isOnline = false
-	return (
-		<>
-			<Header />
-			{isOnline ? <h1>ONLINE</h1> : <h1>Offline</h1>}
-			{isOnline ? (
-				<Section sectionTitle='Movies' data={moviesData} />
-			) : (
-				<Section sectionTitle='Goods' data={goodsData} />
-			)}
-			{isOnline ? <h2>Welcome back</h2> : <h2>Please login</h2>}
-
-			<Wrapper>
-				<h1>Hello world</h1>
-			</Wrapper>
-
-			<Wrapper>
-				<h1>hello</h1>
-			</Wrapper>
-		</>
-	)
+	return <div>App</div>
 }
