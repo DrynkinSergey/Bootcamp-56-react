aca961f02a2a86543d8b34edcde34ff923cb7c03
Sergey <sergsmile29@gmail.com>
2023-07-31 13:09:52 +0300

    final 3 components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import { Flex } from './styles/GlobalStyles'
 import { TodoList } from './components/TodoList/TodoList'
+import { Counter } from './components/Counter/Counter'
+import { ColorPicker } from './components/ColorPicker/ColorPicker'
+import colors from './assets/colors.json'
 export const App = () => {
 	return (
 		<>
+			<Flex $gap='24px' $justify='center' $items='center' $direction='column'>
+				<ColorPicker colors={colors} />
+			</Flex>
+			<Flex $gap='24px' $justify='center' $items='center' $direction='column'>
+				<Counter />
+			</Flex>
 			<Flex $gap='24px' $justify='center' $items='center' $direction='column'>
 				<TodoList />
 			</Flex>
