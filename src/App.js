634e4e1de43cf880c729d8dba79de2f5a40bbb70
Sergey <sergsmile29@gmail.com>
2023-07-31 12:58:58 +0300

    todoList

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import { Flex } from './styles/GlobalStyles'
 import { TodoList } from './components/TodoList/TodoList'
-import { ColorPicker } from './components/ColorPicker/ColorPicker'
-import { Counter } from './components/Counter/Counter'
-import colorsJSON from './assets/colors.json'
-import Modal from './components/Modal/Modal'
 export const App = () => {
 	return (
 		<>
 			<Flex $gap='24px' $justify='center' $items='center' $direction='column'>
-				<ColorPicker message='Hello React' colors={colorsJSON} />
+				<TodoList />
 			</Flex>
 		</>
 	)
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,25 +1,114 @@
+import { nanoid } from 'nanoid'
 import { StyledButton } from '../Counter/Counter.styled'
 import { StyledInput, StyledTodo, StyledTodoList } from './TodoList.styled'
 import todos from './../../assets/todos.json'
 import { Flex } from '../../styles/GlobalStyles'
+import React from 'react'
 
-export const TodoList = () => {
-	return (
-		<div>
-			<StyledTodoList>
-				<Flex $height='auto'>
-					<StyledInput type='text' />
-					<StyledButton>Add</StyledButton>
-				</Flex>
-				{todos.map(item => (
-					<StyledTodo>
-						<input type='checkbox' />
-						<span>{item.todo}</span>
-						<StyledButton size='18px'>Delete</StyledButton>
-					</StyledTodo>
-				))}
-				<button>Clear</button>
-			</StyledTodoList>
-		</div>
-	)
+export class TodoList extends React.Component {
+	state = {
+		todos,
+		newTodoValue: '',
+	}
+	handleAddTodo = () => {
+		this.setState(prev => ({
+			todos: [
+				...prev.todos,
+				{ id: nanoid(), todo: prev.newTodoValue, completed: false },
+			],
+			newTodoValue: '',
+		}))
+	}
+
+	handleDelete = id => {
+		console.log(id)
+		this.setState(prev => ({
+			todos: prev.todos.filter(todo => {
+				return todo.id !== id
+			}),
+		}))
+	}
+	handleChangeInput = e => {
+		this.setState({ newTodoValue: e.target.value })
+	}
+
+	handleToggleTodo = id => {
+		// Тернарне вираження
+		// this.setState(prev => ({
+		// 	todos: prev.todos.map(todo =>
+		// 		todo.id === id ? { ...todo, completed: !todo.completed } : todo
+		// 	),
+		// }))
+		// Традиційне іф елс
+
+		this.setState(prev => ({
+			todos: prev.todos.map(todo => {
+				if (todo.id === id) {
+					return { ...todo, completed: !todo.completed }
+				} else {
+					return todo
+				}
+			}),
+		}))
+	}
+
+	handleClear = () => {
+		this.setState({ todos: [] })
+	}
+
+	render() {
+		const { todos, newTodoValue } = this.state
+		return (
+			<>
+				<StyledTodoList>
+					<Flex $height='auto'>
+						<StyledInput
+							value={newTodoValue}
+							onChange={this.handleChangeInput}
+							type='text'
+						/>
+						<StyledButton onClick={this.handleAddTodo}>Add</StyledButton>
+					</Flex>
+					{todos.map(item => (
+						<StyledTodo key={item.id}>
+							<input
+								checked={item.completed}
+								type='checkbox'
+								onChange={() => this.handleToggleTodo(item.id)}
+							/>
+							<span>{item.todo}</span>
+							<StyledButton
+								onClick={() => this.handleDelete(item.id)}
+								size='18px'
+							>
+								Delete
+							</StyledButton>
+						</StyledTodo>
+					))}
+					<button onClick={this.handleClear}>Clear</button>
+				</StyledTodoList>
+			</>
+		)
+	}
 }
+
+// export const TodoList = () => {
+// 	return (
+// 		<>
+// 			<StyledTodoList>
+// 				<Flex $height='auto'>
+// 					<StyledInput type='text' />
+// 					<StyledButton>Add</StyledButton>
+// 				</Flex>
+// 				{todos.map(item => (
+// 					<StyledTodo>
+// 						<input type='checkbox' />
+// 						<span>{item.todo}</span>
+// 						<StyledButton size='18px'>Delete</StyledButton>
+// 					</StyledTodo>
+// 				))}
+// 				<button>Clear</button>
+// 			</StyledTodoList>
+// 		</>
+// 	)
+// }
