ac4247020dc665d3932b289487d33ed92d86759f
Sergey <sergsmile29@gmail.com>
2023-07-31 11:11:50 +0300

    counter

--- src/App.js
+++ src/App.js
@@ -8,23 +8,7 @@ export const App = () => {
 	return (
 		<>
 			<Flex $gap='24px' $justify='center' $items='center' $direction='column'>
-				<TodoList />
-			</Flex>
-			<Flex $gap='24px' $justify='center' $items='center' $direction='column'>
-				<ColorPicker colors={colors} />
-			</Flex>
-			<Flex $gap='24px' $justify='center' $items='center' $direction='column'>
 				<Counter />
-			</Flex>
-			<Flex $gap='24px' $justify='center' $items='center' $direction='column'>
-				<Modal>
-					<h1>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
-						quisquam perspiciatis autem officia velit laboriosam porro illo ipsa
-						earum enim totam qui laudantium, est alias incidunt exercitationem
-						reprehenderit. Consectetur, culpa.
-					</h1>
-				</Modal>
 			</Flex>
 		</>
 	)
--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import {
 	Flex,
 	FlexContainer,
@@ -5,17 +6,73 @@ import {
 	StyledCounter,
 } from './Counter.styled'
 
-export const Counter = () => {
-	return (
-		<FlexContainer>
-			<StyledCounter>
-				<h1>{0}</h1>
-				<Flex>
-					<StyledButton>minus</StyledButton>
-					<StyledButton>reset</StyledButton>
-					<StyledButton>plus</StyledButton>
-				</Flex>
-			</StyledCounter>
-		</FlexContainer>
-	)
+export class Counter extends React.Component {
+	state = {
+		counter: 0,
+		step: 1,
+	}
+	handleIncrement = () => {
+		// Без попереднього
+		// this.setState({ counter: this.state.counter + 1 })
+
+		// З попереднім станом
+		this.setState(prevState => {
+			return {
+				counter: prevState.counter + prevState.step,
+			}
+		})
+	}
+
+	handleDecrement = () => {
+		// this.setState({ counter: this.state.counter - 1 })
+		this.setState(prevState => ({
+			counter: prevState.counter - prevState.step,
+		}))
+	}
+
+	handleReset = () => {
+		this.setState({ counter: 0, step: 1 })
+	}
+	handleChangeStep = e => {
+		console.log(e.target.value)
+		this.setState({ step: +e.target.value })
+	}
+
+	render() {
+		return (
+			<FlexContainer>
+				<StyledCounter>
+					<input
+						placeholder='change step'
+						value={this.state.step}
+						onChange={this.handleChangeStep}
+					/>
+					<h2 style={{ color: 'white', fontSize: '3rem' }}>
+						{this.state.counter}
+					</h2>
+					<Flex>
+						<StyledButton onClick={this.handleDecrement}>minus</StyledButton>
+						<StyledButton onClick={this.handleReset}>reset</StyledButton>
+						<StyledButton onClick={this.handleIncrement}>plus</StyledButton>
+					</Flex>
+				</StyledCounter>
+			</FlexContainer>
+		)
+	}
 }
+// export const Counter = () => {
+// return (
+// 	<FlexContainer>
+// 		<StyledCounter>
+// 			<h1>{0}</h1>
+// 			<Flex>
+// 				<StyledButton onClick={() => console.log('click')}>
+// 					minus
+// 				</StyledButton>
+// 				<StyledButton>reset</StyledButton>
+// 				<StyledButton>plus</StyledButton>
+// 			</Flex>
+// 		</StyledCounter>
+// 	</FlexContainer>
+// )
+// }
