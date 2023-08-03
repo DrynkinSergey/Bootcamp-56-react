4220d033beaa5e9bc858d7abf164d83621004b4e
Sergey <sergsmile29@gmail.com>
2023-08-04 02:33:56 +0300

    fix

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,16 +1,9 @@
 import React from 'react'
 import { StyledWrapper } from './SearchForm.styled'
 
-export const SearchForm = ({ setQuery }) => {
-	const onSubmit = e => {
-		e.preventDefault()
-		const form = e.currentTarget
-		const query = form.elements.query.value
-		setQuery(query)
-		form.reset()
-	}
+export const SearchForm = () => {
 	return (
-		<StyledWrapper onSubmit={onSubmit}>
+		<StyledWrapper>
 			<input autoComplete='off' autoFocus='on' type='text' name='query' />
 			<button>Search</button>
 		</StyledWrapper>
