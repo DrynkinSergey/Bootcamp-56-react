c5db78ded51869b1630a6ff3a864b13b0ef88396
Sergey <sergsmile29@gmail.com>
2023-08-07 23:34:44 +0300

    fix counter

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -26,28 +26,18 @@ export const Counter = () => {
 
 	const handleIncrement = () => {
 		setCounter(prev => prev + step)
-		// this.setState(prevState => {
-		// 	return {
-		// 		counter: prevState.counter + prevState.step,
-		// 	}
-		// })
 	}
 
 	const handleDecrement = () => {
 		setCounter(prev => prev - step)
-		// this.setState(prevState => ({
-		// 	counter: prevState.counter - prevState.step,
-		// }))
 	}
 
 	const handleReset = () => {
-		// this.setState({ counter: 0, step: 1 })
 		setCounter(0)
 		setStep(1)
 	}
 	const handleChangeStep = e => {
 		setStep(+e.target.value)
-		// this.setState({ step: +e.target.value })
 	}
 	return (
 		<FlexContainer>
@@ -63,85 +53,3 @@ export const Counter = () => {
 		</FlexContainer>
 	)
 }
-
-// export class Counter extends React.Component {
-// 	state = {
-// 		counter: 0,
-// 		step: 1,
-// 	}
-
-// 	componentDidMount() {
-// 		console.log('Counter is mount')
-// 	}
-
-// 	componentDidUpdate(prevProps, prevState) {
-// 		if (prevState.counter !== this.state.counter) {
-// 			console.log('Counter was updated')
-// 		}
-// 		if (prevState.step !== this.state.step) {
-// 			console.log('Step was updated')
-// 		}
-// 		// if (prevState.step) console.log('Updated counter or step')
-// 		if (this.state.counter === 5 && this.state.step !== 10) {
-// 			console.log('Update step')
-// 			this.setState({ step: 10 })
-// 		}
-// 	}
-
-// updateToTen = () => {
-// 	this.setState({ step: 10 })
-// }
-
-// handleIncrement = () => {
-// 	this.setState(prevState => {
-// 		return {
-// 			counter: prevState.counter + prevState.step,
-// 		}
-// 	})
-// }
-
-// handleDecrement = () => {
-// 	this.setState(prevState => ({
-// 		counter: prevState.counter - prevState.step,
-// 	}))
-// }
-
-// handleReset = () => {
-// 	this.setState({ counter: 0, step: 1 })
-// }
-// handleChangeStep = e => {
-// 	this.setState({ step: +e.target.value })
-// }
-
-// 	render() {
-// 		return (
-// 			<FlexContainer>
-// 				<StyledCounter>
-// 					<input placeholder='change step' value={this.state.step} onChange={this.handleChangeStep} />
-// 					<h2 style={{ color: 'white', fontSize: '3rem' }}>{this.state.counter}</h2>
-// 					<Flex>
-// 						<StyledButton onClick={this.handleDecrement}>minus</StyledButton>
-// 						<StyledButton onClick={this.handleReset}>reset</StyledButton>
-// 						<StyledButton onClick={this.handleIncrement}>plus</StyledButton>
-// 					</Flex>
-// 				</StyledCounter>
-// 			</FlexContainer>
-// 		)
-// 	}
-// }
-// export const Counter = () => {
-// return (
-// 	<FlexContainer>
-// 		<StyledCounter>
-// 			<h1>{0}</h1>
-// 			<Flex>
-// 				<StyledButton onClick={() => console.log('click')}>
-// 					minus
-// 				</StyledButton>
-// 				<StyledButton>reset</StyledButton>
-// 				<StyledButton>plus</StyledButton>
-// 			</Flex>
-// 		</StyledCounter>
-// 	</FlexContainer>
-// )
-// }
