96aa64352a03c905038131160659505ab7e1336b
Sergey <sergsmile29@gmail.com>
2023-08-09 00:56:27 +0300

    plan

--- src/Services/api.js
+++ src/Services/api.js
@@ -2,15 +2,3 @@ import axios from 'axios'
 // 'https://dummyjson.com/posts/search'
 
 axios.defaults.baseURL = 'https://dummyjson.com/'
-
-export const fetchPosts = async params => {
-	let url = params.q ? 'posts/search' : 'posts'
-
-	const { data } = await axios.get(url, {
-		params: {
-			limit: 15,
-			...params,
-		},
-	})
-	return data
-}
