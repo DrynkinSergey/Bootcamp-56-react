01e40669fcc040635b29bc0bee7aceff894a5aeb
Sergey <sergsmile29@gmail.com>
2023-08-09 23:45:19 +0300

    PLAN

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { GlobalStyles } from './styles/GlobalStyles'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
 	<>
-		<App title='Hello optimization' />
+		<App />
 		<GlobalStyles />
 		<ToastContainer autoClose={1000} />
 	</>
