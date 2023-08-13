807233396f6660cb9b938dcb08b34baf0f6e988e
Sergey <sergsmile29@gmail.com>
2023-08-13 23:58:24 +0300

    redux init

--- src/hooks/useModal.js
+++
@@ -1,9 +0,0 @@
-import { useState } from 'react'
-
-export const useModal = () => {
-	const [isOpen, setIsOpen] = useState(false)
-	const openModal = () => setIsOpen(true)
-	const closeModal = () => setIsOpen(false)
-	const toggle = () => setIsOpen(prev => !prev)
-	return { isOpen, toggle, openModal, closeModal }
-}
--- src/pages/NotFound.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-
-const NotFound = () => {
-	return <div>Page is not found</div>
-}
-export default NotFound
