10968d0cfcd19a170a2cc473e8df165cb992d498
Sergey <sergsmile29@gmail.com>
2023-08-14 10:57:52 +0300

    counter redux

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -1,18 +1,30 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { styled } from 'styled-components'
 
 export const Counter = () => {
-	const { counter, step } = useSelector(state => state.counter)
+	const { counter } = useSelector(state => state.counter)
+	const dispatch = useDispatch()
+
+	const increment = () => {
+		dispatch({ type: 'INC' })
+	}
+	const reset = () => {
+		dispatch({ type: 'RESET' })
+	}
+	const incrementBy10 = () => {
+		dispatch({ type: 'INC_BY_10', payload: 10 })
+	}
 
 	return (
 		<Flex>
 			<h1>Counter Redux</h1>
 			<h2>{counter}</h2>
 			<div>
-				<button>Minus</button>
-				<button>Reset</button>
-				<button>Plus</button>
+				<button onClick={() => dispatch({ type: 'DEC' })}>Minus</button>
+				<button onClick={reset}>Reset</button>
+				<button onClick={increment}>Plus</button>
+				<button onClick={incrementBy10}>Plus 10</button>
 			</div>
 		</Flex>
 	)
--- src/redux/counter/reducer.js
+++ src/redux/counter/reducer.js
@@ -1,10 +1,31 @@
 const initialState = {
-	counter: 222,
+	counter: 2,
 	step: 1,
 }
 
 export const counterReducer = (state = initialState, action) => {
+	const { counter, step } = state
 	switch (action.type) {
+		case 'INC':
+			return {
+				...state,
+				counter: counter + step,
+			}
+		case 'DEC':
+			return {
+				...state,
+				counter: counter - step,
+			}
+		case 'RESET':
+			return {
+				...state,
+				counter: 0,
+			}
+		case 'INC_BY_10':
+			return {
+				...state,
+				counter: counter + action.payload,
+			}
 		default:
 			return state
 	}
