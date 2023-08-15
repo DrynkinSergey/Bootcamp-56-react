93e072d29cafb2705decce082476b1172e0d2704
Sergey <sergsmile29@gmail.com>
2023-08-15 12:56:12 +0300

    redux persist

--- src/index.js
+++ src/index.js
@@ -5,15 +5,18 @@ import 'react-toastify/dist/ReactToastify.css'
 import ReactDOM from 'react-dom/client'
 import { GlobalStyles } from './styles/GlobalStyles'
 import { Provider } from 'react-redux'
-import { store } from './redux/store'
+import { persistor, store } from './redux/store'
+import { PersistGate } from 'redux-persist/integration/react'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
 	// Огортаємо Провайдером , передаємо пропс store для того,щоб працював редакс
 	// Імпорт store йде з файлу стор в редаксі
-	<Provider store={store}>
-		<App />
-		<GlobalStyles />
-		<ToastContainer autoClose={1000} />
-	</Provider>
+	<PersistGate loading={null} persistor={persistor}>
+		<Provider store={store}>
+			<App />
+			<GlobalStyles />
+			<ToastContainer autoClose={1000} />
+		</Provider>
+	</PersistGate>
 )
