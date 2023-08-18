a39fd3988c0ba428173c3fd5752ea4ed27b33259
Sergey <sergsmile29@gmail.com>
2023-08-18 09:57:08 +0300

    first variant for select notCompletedTodo

--- src/components/Todo/Filter.jsx
+++ src/components/Todo/Filter.jsx
@@ -1,14 +1,19 @@
+import { useSelector } from 'react-redux'
+import { selectNotCompleted, selectTodos } from '../../redux/selectors'
+
 export const Filter = () => {
+	const todos = useSelector(selectTodos)
+	const notCompleted = useSelector(state => selectNotCompleted(state, todos))
 	const filter = ''
 	const setClasses = type => (filter === type ? `${classes} text-blue-600` : classes)
 	const classes = 'hover:text-white cursor-pointer'
 	return (
 		<li className='bg-darkMain list-none  py-4 flex-col md:flex-row  text-white/40 flex justify-between px-8 items-center'>
-			{'0' === 0 ? (
+			{notCompleted === 0 ? (
 				<span className='md:order-1 order-1'>All is complete!</span>
 			) : (
 				<span className='md:order-1 order-1'>
-					<span className='text-blue-500'>{'0'}</span> items left
+					<span className='text-blue-500'>{notCompleted}</span> items left
 				</span>
 			)}
 			<div className='flex gap-4 order-3 mt-4 md:mt-0  md:order-2'>
--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -4,6 +4,7 @@ export const selectPosts = state => state.postList.posts
 export const selectLoading = state => state.postList.loading
 
 //Fav selectors
+
 export const selectFavPosts = state => state.favourite.favouritePosts
 
 //TOdos selectors
@@ -11,3 +12,8 @@ export const selectFavPosts = state => state.favourite.favouritePosts
 export const selectTodos = state => state.todoList.todos
 export const selectIsLoading = state => state.todoList.loading
 export const selectError = state => state.todoList.error
+
+export const selectNotCompleted = (state, todos) => {
+	console.log(state, todos)
+	return todos.reduce((acc, curr) => (curr.completed ? acc : acc + 1), 0)
+}
